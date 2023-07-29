65637dd047fa512bea53df862c6ab388603bef65
WB_777 <antariuzz@gmail.com>
2023-07-29 15:46:35 +0300

    add back data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React, {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAngleLeft, faAngleRight} from "@fortawesome/free-solid-svg-icons";
-import  axios from "axios"
+import axios from "axios"
 import "./index.css";
-import $ from "jquery";
 
 const Slider = () => {
     const slideRef = useRef(null);
     const [loadingProgress, setLoadingProgress] = useState(0);
+    const [carousel, setCarousel] = useState([]);
 
     const handleClickNext = () => {
         let items = slideRef.current.querySelectorAll(".item");
@@ -30,7 +30,6 @@ const Slider = () => {
             }, (i + 1) * 450);
 
         }
-        getData();
     };
 
     const handleClickPrev = () => {
@@ -41,84 +40,21 @@ const Slider = () => {
         window.location.href = url;
     };
 
-    function getAllUsers() {
-        const response = $.ajax({
-            method: "GET",
-            url: "http://localhost:8080/carousel/get-all-carousel",
-            dataType: 'json',
-            contentType: 'application/json',
-        });
-        console.log(response);
-    }
-    function getData(){
+    useEffect(() => {
+        getData();
+    }, []);
+
+    function getData() {
+
         axios.get("http://localhost:8080/carousel/get-all-carousel")
             .then(
                 (result) => {
-                    console.log(result)
+                    setCarousel(result.data.carousel);
                 }
             )
     }
 
-    const data1 =  getData();
-    const data = [
-        {
-            id: 1,
-            imgUrl: "http://localhost:5173/img.png",
-            desc: "PETR IVANOV",
-            login: "Login: admin@gmail.com",
-            pass: "Password: 1234",
-            name: "Kino-CMS",
-            url: "http://localhost:8080/Vilki_Palki_Admin/login",
-        },
-        {
-            id: 2,
-            imgUrl:
-                "https://i.postimg.cc/bw6KxhLf/pexels-eberhard-grossgasteiger-1062249.jpg",
-            desc: "PETR IVANOV",
-            login: "Login: admin@gmail.com",
-            pass: "Password: 1234",
-            name: "Kino-CMS",
-            url: "http://localhost:8080/Vilki_Palki_Admin/login",
-        }, {
-            id: 3,
-            imgUrl:
-                "https://i.postimg.cc/bw6KxhLf/pexels-eberhard-grossgasteiger-1062249.jpg",
-            desc: "PETR IVANOV",
-            login: "Login: admin@gmail.com",
-            pass: "Password: 1234",
-            name: "Kino-CMS",
-            url: "http://localhost:8080/Vilki_Palki_Admin/login",
-        },
-        {
-            id: 4,
-            imgUrl:
-                "https://i.postimg.cc/CMkTW9Mb/pexels-eberhard-grossgasteiger-572897.jpg",
-            desc: "PETR IVANOV",
-            login: "Login: admin@gmail.com",
-            pass: "Password: 1234",
-            name: "Kino-CMS",
-            url: "http://localhost:8080/Vilki_Palki_Admin/login",
-        },
-        {
-            id: 5,
-            imgUrl: "https://i.postimg.cc/6qdkn4bM/pexels-joyston-judah-933054.jpg",
-            desc: "PETR IVANOV",
-            login: "Login: admin@gmail.com",
-            pass: "Password: 1234",
-            name: "Kino-CMS",
-            url: "http://localhost:8080/Vilki_Palki_Admin/login",
-        },
-        {
-            id: 6,
-            imgUrl:
-                "http://localhost:5173/img2.png",
-            desc: "PETR IVANOV",
-            login: "Login: admin@gmail.com",
-            pass: "Password: 1234",
-            name: "Kino-CMS",
-            url: "https://www.baeldung.com/spring-boot-internationalization",
-        },
-    ];
+    const data = carousel;
 
     return (
         <div className="container">
@@ -133,11 +69,11 @@ const Slider = () => {
                     >
                         <div className="content">
                             <div hidden={true}>{item.id}</div>
-                            <div className="name">{item.name}</div>
+                            <div className="name">{item.projectName}</div>
                             <div className="des">{item.desc}</div>
                             <div className="des">{item.login}</div>
-                            <div className="des">{item.pass}</div>
-                            <button role={"button"} onClick={() => handClickUrl(item.url)}>See more</button>
+                            <div className="des">{item.password}</div>
+                            <button role={"button"} onClick={() => handClickUrl(item.projectUrl)}>See more</button>
                         </div>
                     </div>
                 ))}
